c40615941bc1fb0aa58db66644767981ff57c58e
Jon Brennecke <jpbrennecke@gmail.com>
2018-05-30 02:29:09 -0700

    forgot to rename variables

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
+if [ -z "$OPENSSL_KEY" ]; then
+  echo "You must set the environment variable OPENSSL_KEY."
+  exit 1
+fi
+
+if [ -z "$OPENSSL_IV" ]; then
+  echo "You must set the environment variable OPENSSL_IV."
+  exit 1
+fi
+
 openssl aes-256-cbc \
-    -K $encrypted_deae71ea1931_key \
-    -iv $encrypted_deae71ea1931_iv \
+    -K $OPENSSL_KEY \
+    -iv $OPENSSL_IV \
     -in ./ssh-keys/deploy_key.enc \
     -out ./ssh-keys/deploy_key -d
 chmod 400 ./ssh-keys/deploy_key
